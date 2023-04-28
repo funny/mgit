5741022ee705ef557bdc0b007ae289749046214d
顽强 <foobar@xmfunny.com>
2023-04-28 09:44:37 +0800

    <fix> 修复部分命令行输出乱码问题

--- cli/src/utils/logger.rs
+++ cli/src/utils/logger.rs
@@ -1,4 +1,4 @@
-use owo_colors::OwoColorize;
+use console::Style;
 use std::path::Path;
 
 use crate::utils::path::display_path;
@@ -15,29 +15,33 @@ pub fn new(str: impl AsRef<str>) {
 }
 
 pub fn dir_not_found(path: impl AsRef<Path>) {
+    let magenta_bold = Style::new().magenta().bold();
     println!(
         "Directory {} not found!",
-        path.as_ref().display().bold().magenta()
+        magenta_bold.apply_to(path.as_ref().display())
     );
 }
 
 pub fn dir_already_inited(path: impl AsRef<Path>) {
+    let magenta_bold = Style::new().magenta().bold();
     println!(
         "{} already inited, try {} instead!",
         path.as_ref().display(),
-        "--force".bold().magenta()
+        magenta_bold.apply_to("--force")
     );
 }
 
 pub fn update_config_succ() {
-    println!("{} update", ".gitrepos".bold().magenta());
+    let magenta_bold = Style::new().magenta().bold();
+    println!("{} update", magenta_bold.apply_to(".gitrepos"));
 }
 
 pub fn config_file_not_found() {
+    let magenta_bold = Style::new().magenta().bold();
     println!(
         "{} not found, try {} instead!",
-        ".gitrepos".bold().magenta(),
-        "init".bold().magenta()
+        magenta_bold.apply_to(".gitrepos"),
+        magenta_bold.apply_to("init")
     );
 }
 
@@ -50,11 +54,10 @@ pub fn remvoe_file_failed(path: impl AsRef<Path>, err: anyhow::Error) {
 }
 
 pub fn remvoe_file_succ(path: impl AsRef<Path>) {
+    let magenta_bold = Style::new().magenta().bold();
     println!(
         "  {}: removed ",
-        display_path(path.as_ref().to_str().unwrap())
-            .bold()
-            .magenta()
+        magenta_bold.apply_to(display_path(path.as_ref().to_str().unwrap()))
     );
 }
 
@@ -63,60 +66,65 @@ pub fn remove_none_repo_succ() {
 }
 
 pub fn remove_one_repo_succ() {
-    println!(
-        "{} repository is removed.\n",
-        "1".to_string().bold().green()
-    );
+    let green_bold = Style::new().green().bold();
+    println!("{} repository is removed.\n", green_bold.apply_to("1"));
 }
 
 pub fn remove_multi_repos_succ(count: u32) {
+    let green_bold = Style::new().green().bold();
     println!(
         "{} repositories are removed.\n",
-        count.to_string().bold().green()
+        green_bold.apply_to(count.to_string())
     );
 }
 
 pub fn command_start(command: impl AsRef<str>, path: impl AsRef<Path>) {
+    let magenta_bold = Style::new().magenta().bold();
     println!(
         "{} in {}",
         command.as_ref(),
-        path.as_ref().display().bold().magenta()
+        magenta_bold.apply_to(path.as_ref().display())
     );
 }
 
 pub fn error_statistics(prefix: impl AsRef<str>, count: usize) {
+    let red_bold = Style::new().red().bold();
     match count {
         0 => println!("{} finished! 0 error(s).", prefix.as_ref()),
         _ => println!(
             "{} finished! {} error(s).",
             prefix.as_ref(),
-            count.to_string().bold().red()
+            red_bold.apply_to(count.to_string())
         ),
     }
     println!("");
 }
 
 pub fn error_detail(rel_path: impl AsRef<str>, error: &anyhow::Error) {
+    let red = Style::new().red();
+    let magenta_bold = Style::new().magenta().bold();
     let mut err_msg = String::new();
+
     for e in error.chain() {
         err_msg += &e.to_string();
     }
 
     eprintln!(
         "{} {}",
-        display_path(&rel_path).bold().magenta(),
-        err_msg.trim().red()
+        magenta_bold.apply_to(display_path(&rel_path)),
+        red.apply_to(err_msg.trim())
     );
     println!("");
 }
 
 pub fn fmt_untrack_desc(path: impl AsRef<Path>, desc: impl AsRef<str>) -> String {
+    let blue = Style::new().blue();
+    let magenta_bold = Style::new().magenta().bold();
+
     format!(
         "{}: {} untracked",
-        display_path(path.as_ref().to_str().unwrap())
-            .bold()
-            .magenta(),
-        desc.as_ref().blue(),
+        magenta_bold.apply_to(display_path(path.as_ref().to_str().unwrap())),
+        blue.apply_to(desc.as_ref()),
     )
 }
 
@@ -134,10 +142,11 @@ pub fn fmt_spinner_desc(
     rel_path: impl AsRef<str>,
     desc: impl AsRef<str>,
 ) -> String {
+    let magenta_bold = Style::new().magenta().bold();
     format!(
         "{:>9} {}: {}",
         prefix.as_ref(),
-        display_path(&rel_path).bold().magenta(),
+        magenta_bold.apply_to(display_path(&rel_path)),
         desc.as_ref()
     )
 }
@@ -147,16 +156,20 @@ pub fn fmt_spinner_finished_prefix(
     rel_path: impl AsRef<str>,
     is_succ: bool,
 ) -> String {
+    let green_bold = Style::new().green().bold();
+    let red_bold = Style::new().red().bold();
+    let magenta_bold = Style::new().magenta().bold();
+
     let sign = match is_succ {
-        true => "√".bold().green().to_string(),
-        false => "x".bold().red().to_string(),
+        true => green_bold.apply_to("√"),
+        false => red_bold.apply_to("x"),
     };
 
     format!(
         "{} {} {}",
-        sign.bold().green(),
+        sign,
         prefix.as_ref(),
-        display_path(rel_path.as_ref()).bold().magenta()
+        magenta_bold.apply_to(display_path(rel_path.as_ref()))
     )
 }
 
@@ -165,65 +178,76 @@ pub fn fmt_tracking_succ_desc(
     local_branch: impl AsRef<str>,
     remote_desc: impl AsRef<str>,
 ) -> String {
+    let blue = Style::new().blue();
+    let magenta_bold = Style::new().magenta().bold();
+
     format!(
         "{}: {} -> {}",
-        display_path(rel_path).bold().magenta(),
-        local_branch.as_ref().blue(),
-        remote_desc.as_ref().blue()
+        magenta_bold.apply_to(display_path(rel_path)),
+        blue.apply_to(local_branch.as_ref()),
+        blue.apply_to(remote_desc.as_ref())
     )
 }
 
 pub fn fmt_tracking_failed_desc(rel_path: impl AsRef<str>, remote_desc: impl AsRef<str>) -> String {
+    let blue = Style::new().blue();
+    let red = Style::new().red();
+    let magenta_bold = Style::new().magenta().bold();
+
     format!(
         "{}: {} {} {}",
-        display_path(rel_path).bold().magenta(),
-        "track failed,".red(),
-        remote_desc.as_ref().blue(),
-        "not found!".red()
+        magenta_bold.apply_to(display_path(rel_path)),
+        red.apply_to("track failed,"),
+        blue.apply_to(remote_desc.as_ref()),
+        red.apply_to("not found!")
     )
 }
 
 pub fn fmt_remote_not_found(remote_ref: impl AsRef<str>) -> String {
-    format!("remote {} not found.", remote_ref.as_ref().blue())
+    let blue = Style::new().blue();
+    format!("remote {} not found.", blue.apply_to(remote_ref.as_ref()))
 }
 
 pub fn fmt_checkouting(branch: impl AsRef<str>) -> String {
-    format!("checkout {}...", branch.as_ref().blue())
+    let blue = Style::new().blue();
+    format!("checkout {}...", blue.apply_to(branch.as_ref()))
 }
 
 pub fn fmt_changes_desc(len: usize) -> String {
-    format!(", changes({})", len).red().to_string()
+    let red = Style::new().red();
+    red.apply_to(format!(", changes({})", len)).to_string()
 }
 
 pub fn fmt_commit_desc(ahead: impl AsRef<str>, behind: impl AsRef<str>) -> String {
     let ahead = ahead.as_ref();
     let behind = behind.as_ref();
+    let yellow = Style::new().yellow();
 
-    match (ahead, behind) {
+    let commit_str = match (ahead, behind) {
         ("0", "0") => String::new(),
-        (_, "0") => String::from(", ") + &format!("commits({}↑)", ahead).yellow().to_string(),
-        ("0", _) => String::from(", ") + &format!("commits({}↓)", behind).yellow().to_string(),
-        _ => {
-            String::from(", ")
-                + &format!("commits({}↑{}↓)", ahead, behind)
-                    .yellow()
-                    .to_string()
-        }
-    }
+        (_, "0") => format!("commits({}↑)", ahead),
+        ("0", _) => format!("commits({}↓)", behind),
+        _ => format!("commits({}↑{}↓)", ahead, behind),
+    };
+
+    String::from(", ") + &yellow.apply_to(commit_str).to_string()
 }
 
 pub fn fmt_unknown_revision_desc() -> String {
-    format!(", {}", "unknown revision".yellow())
+    let yellow = Style::new().yellow();
+    format!(", {}", yellow.apply_to("unknown revision"))
 }
 
 pub fn fmt_update_to_desc(desc: impl AsRef<str>) -> String {
-    format!("{} {}", "update to".green(), desc.as_ref())
+    let green = Style::new().green();
+    format!("{} {}", green.apply_to("update to"), desc.as_ref())
 }
 
 pub fn fmt_update_to_date_desc(branch_log: impl AsRef<str>) -> String {
+    let gray = Style::new().color256(245);
     format!(
         "already update to date. {}",
-        branch_log.as_ref().bright_black()
+        gray.apply_to(branch_log.as_ref())
     )
 }
 
@@ -232,9 +256,10 @@ pub fn fmt_diff_desc(
     commit_desc: impl AsRef<str>,
     changes_desc: impl AsRef<str>,
 ) -> String {
+    let blue = Style::new().blue();
     format!(
         "{}{}{}",
-        remote_desc.as_ref().blue(),
+        blue.apply_to(remote_desc.as_ref()),
         commit_desc.as_ref(),
         changes_desc.as_ref(),
     )
