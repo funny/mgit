7465eee8a310302fa3f45143a3ef2815b0b72eb2
BppleMan <bppleman@gmail.com>
2023-06-12 18:34:00 +0800

    <misc>去掉log的格式前缀

--- mgit-cli/src/main.rs
+++ mgit-cli/src/main.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 use log::LevelFilter;
 use log4rs::append::console::ConsoleAppender;
 use log4rs::config::{Appender, Logger, Root};
+use log4rs::encode::pattern::PatternEncoder;
 use log4rs::Config;
 use mgit::ops;
 
@@ -26,7 +27,9 @@ fn main() {
 }
 
 fn init_log() {
-    let stdout = ConsoleAppender::builder().build();
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{m}{n}")))
+        .build();
 
     let config = Config::builder()
         .appender(Appender::builder().build("stdout", Box::new(stdout)))
