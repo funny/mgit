70559d21a0a50aa95ea1705ce62578e7703fd919
顽强 <foobar@xmfunny.com>
2024-02-02 10:52:45 +0800

    <feature> 支持仓库配置 sparse-checkout-dirs 字段

--- core/src/ops/sync.rs
+++ core/src/ops/sync.rs
@@ -365,7 +365,12 @@ fn inner_exec(
             // reset --hard
             exec_reset(input_path, repo_info, progress, ResetType::Hard)
         }
-    }
+    }?;
+
+    match repo_info.toml_repo.sparse_checkout_dirs.as_ref(){
+        Some(dirs) =>  git::sparse_checkout_set(&full_path,dirs),
+        None=>git::sparse_checkout_disable(&full_path)
+     }
 }
 
 fn exec_init(
@@ -420,7 +425,9 @@ fn exec_reset(
         ResetType::Mixed => "--mixed",
         ResetType::Hard => "--hard",
     };
-    git::reset(full_path, reset_type, remote_ref_str)
+
+    git::reset(&full_path, reset_type, remote_ref_str)
+
 }
 
 fn exec_stash(
